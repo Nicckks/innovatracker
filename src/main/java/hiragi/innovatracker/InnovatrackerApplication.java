dd43d071b3386acfd7bde84e68069988854e6bfa
João Nicolau Fill da Silva <joao.nicolau@dinfor.uniceub>
2026-08-06 20:36:04 -0300

    feat: Parte 1 do projeto realizado em sala de aula

---
+++ src/main/java/hiragi/innovatracker/InnovatrackerApplication.java
@@ -0,0 +1,13 @@
+package hiragi.innovatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovatrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovatrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hiragi/innovatracker/InnovatrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package hiragi.innovatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovatrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
